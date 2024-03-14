4d6daac0d37feec09d772aead39a4137d2ddc53c
harsha <sunnyharsha320@gmila.com>
2024-03-14 14:32:29 +0530

    Small changes

--- Automate-Web-Application/src/main/java/flipcard/App.java
+++ Automate-Web-Application/src/main/java/flipcard/App.java
@@ -1,18 +1,19 @@
 package flipcard;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import java.util.List;
-import java.util.concurrent.TimeUnit;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class App {
 
@@ -20,8 +21,8 @@ public class App {
 
 	@BeforeTest
 	public void setupBraveWebDriver() {
-		WebDriverManager.edgedriver().setup();
-		wd = new EdgeDriver();
+		WebDriverManager.chromedriver().setup();
+		wd = new ChromeDriver();
 		wd.manage().window().maximize();
 		wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	}
@@ -31,7 +32,7 @@ public class App {
 		wd.get("https://www.flipkart.com/");
 		Thread.sleep(5000);
 		measurePageLoadTime();
-		searchProduct("iPhone 15", "Mobile");
+		searchProduct("iPhone 15");
 		checkImageVisibility();
 		checkPageScroll();
 		checkContentRefreshFrequency();
@@ -53,7 +54,7 @@ public class App {
 		System.out.println("Page Load Time: " + loadTime + " milliseconds");
 	}
 
-	private void searchProduct(String productName, String category) throws InterruptedException {
+	private void searchProduct(String productName) throws InterruptedException {
 		WebElement searchBox = wd.findElement(By.name("q"));
 		searchBox.sendKeys(productName, Keys.ENTER);
 		Thread.sleep(5000);
